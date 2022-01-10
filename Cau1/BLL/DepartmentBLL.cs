e76afe12f38fc4902f05a87b29308de1e8349207
Hau Nguyen <nthctqn@gmail.com>
2022-01-10 20:56:59 +0700

    Bussiness Layer

--- Cau1/BLL/DepartmentBLL.cs
+++ Cau1/BLL/DepartmentBLL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Cau1.DAO;
+using Cau1.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,12 @@ namespace Cau1.BLL
 {
     public class DepartmentBLL
     {
+        DepartmentDAO dao = new DepartmentDAO();
+        public List<DepartmentDTO> ReadDepartmentList()
+        {
+            List<DepartmentDTO> lstDepartment = dao.ReadDepartmentList();
+            return lstDepartment;
+        }
+
     }
 }
